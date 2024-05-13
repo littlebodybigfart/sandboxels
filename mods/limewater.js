c43577ec1e8b9e10e97009dfaef65bce4660f6bc
littlebodybigfart <158759469+littlebodybigfart@users.noreply.github.com>
2024-05-13 08:50:15 +0300

    Update limewater.js

--- mods/limewater.js
+++ mods/limewater.js
@@ -1,8 +1,9 @@
 // first time making a mod lowkey nervous dk why
 // **don't expect a single line of this to work. seriously.**
+// and a thank you to the discord for actually trying to help this monstrosity run properly
 
-elements.slaked_lime.reactions.water.chance = 0.4 
-elements.slaked_lime.reactions.water = { "elem1":"limewater", "elem2": "limewater"};
+if (!elements.bone.reactions) { elements.bone.reactions = {} }
+elements.slaked_lime.reactions.water = { "elem1": "limewater", chance:0.4 };
 
 elements.limewater = {
   color: ["#3baaff"],
@@ -13,9 +14,9 @@ elements.limewater = {
   burn: 100,
   burnInto: ["steam, slaked_lime"],
   tempLow: 0,
-  reactions: {"carbon_dioxide": {elem1: "calcium_carbonate_solution", elem2: "" },
+  reactions: {"carbon_dioxide": {elem1: "calcium_carbonate_solution", elem2: "null" },
              },
-};
+}
 elements.calcium_carbonate_solution = {
   color: ["#ffffff"],
   behavior: behaviors.LIQUID,
@@ -27,16 +28,17 @@ elements.calcium_carbonate_solution = {
 }
 elements.calcium_carbonate = {
   color: ["#ffffff, #e3e3e3"],
-  behavior: behaviors.LIQUID,
+  behavior: behaviors.POWDER,
   category: "liquids",
   state: "liquid",
   density: 2710,
   burn: 825,
-  burnInto: ["molten"],
+  burnInto: ["molten_calcium_carbonate"],
 }
 
 elements.molten_calcium_carbonate = {
     color: ["#f5190a", "#d4180b", "#f5190a", "#423f3e"],
+    behavior: behaviors.POWDER,
     category: "powders",
     state: "solid",
     density: 2710,
@@ -44,4 +46,3 @@ elements.molten_calcium_carbonate = {
     tempLow: 820,
     stateLow: "calcium_carbonate",
 }
-
